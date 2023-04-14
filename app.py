0f1b8fcc6af3cb1149fb3955c9392987000c71a2
Peter Lombardo <peterllombardo@gmail.com>
2023-04-14 15:22:41 -0400

    code commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolBarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "topsecret"
+
+debug = DebugToolBarExtension(app)
+
+@app.route("/")
+def inputs():
+
+    prompts = story.prompts
+    return render_template("inputs.html", prompts=prompts)
+
+@app.route("/story")
+def make_story():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
